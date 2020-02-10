6bfe3d7ce0b4e5b03eada750ce379e14e016e61a
Nguyen Thi Hang B <nguyen.thi.hangb@sun-asterisk.com>
2020-02-10 13:36:33 +0700

    Init project rails-react

---
+++ frontend/app/components/Header/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+
+import Img from './Img';
+import NavBar from './NavBar';
+import HeaderLink from './HeaderLink';
+import Banner from './banner.jpg';
+import messages from './messages';
+
+function Header() {
+  return (
+    <div>
+      <NavBar>
+        <HeaderLink to="/">
+          <FormattedMessage {...messages.home} />
+        </HeaderLink>
+        <HeaderLink to="/features">
+          <FormattedMessage {...messages.features} />
+        </HeaderLink>
+      </NavBar>
+    </div>
+  );
+}
+
+export default Header;
